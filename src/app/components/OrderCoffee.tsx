9cd42b3e47f26dc21c627c003f47bccee10ba326
bytesourav-code <bytesourav@gmail.com>
2025-06-08 10:50:40 +0530

    feat: added responsive Order Your Favourite Coffee section

--- src/app/components/OrderCoffee.tsx
+++ src/app/components/OrderCoffee.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import { Button } from "@headlessui/react";
+import Image from "next/image";
+import React from "react";
 
 const OrderCoffee = () => {
   return (
-    <div>
-      Order Coffee
+    <div className="bg-gray-100">
+      <div className="container grid grid-cols-1 md:grid-cols-2 items-center py-10 max-w-4xl mx-auto">
+        <div>
+          <Image
+            src="/coffee/three-coffee-mug.png"
+            alt="Coffee Mug"
+            width={600}
+            height={600}
+          />
+        </div>
+        <div className="ml-6 md:ml-10">
+          <h1 className="text-3xl md:text-5xl font-bold mt-2">
+            Order Your Favourite Coffee
+          </h1>
+          <p className="text-base md:text-lg text-gray-500 py-2">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam
+            ea suscipit eos magni quo, tenetur doloribus eveniet ut dicta,
+            aliquam consequuntur porro labore voluptatem odio voluptatum dolor
+            vero qui. Quaerat.
+          </p>
+          <Button className="bg-gray-900 text-white px-6 py-3 rounded-lg shadow-lg mt-4 md:mt-10">
+            ORDER NOW
+          </Button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default OrderCoffee
+export default OrderCoffee;
